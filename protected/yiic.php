c505ebc121503dfcebb6b14b5b1f82838a7b8e59
Spiros Kabasakalis <kabasakalis@gmail.com>
2012-04-21 15:53:22 -0700

    First commit

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../frameworks/yii_1.1.10/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
